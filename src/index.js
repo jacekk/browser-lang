c551c94ec7934278aa19e6d8213275b6d790da2e
jacekk <jacek.kapias@amsterdamstandard.com>
2022-10-27 12:05:36 +0200

    chore: ran Prettier on source files;

--- src/index.js
+++ src/index.js
@@ -1,65 +1,63 @@
 function startsWith(string, target, position) {
-  const { length } = string
-  position = position == null ? 0 : position
-  if (position < 0) {
-    position = 0
-  } else if (position > length) {
-    position = length
-  }
-  target = `${target}`
-  return string.slice(position, position + target.length) == target
+	const { length } = string;
+	position = position == null ? 0 : position;
+	if (position < 0) {
+		position = 0;
+	} else if (position > length) {
+		position = length;
+	}
+	target = `${target}`;
+	return string.slice(position, position + target.length) == target;
 }
 
 function getBrowserLang() {
-  if (typeof window === "undefined") {
-    return null
-  }
+	if (typeof window === 'undefined') {
+		return null;
+	}
 
-  const lang =
-    (window.navigator.languages && window.navigator.languages[0]) ||
-    window.navigator.language ||
-    window.navigator.browserLanguage ||
-    window.navigator.userLanguage ||
-    window.navigator.systemLanguage ||
-    null
+	const lang =
+		(window.navigator.languages && window.navigator.languages[0]) ||
+		window.navigator.language ||
+		window.navigator.browserLanguage ||
+		window.navigator.userLanguage ||
+		window.navigator.systemLanguage ||
+		null;
 
-  return lang
+	return lang;
 }
 
 function normalizeCode(code) {
-  return code.toLowerCase().replace(/-/, "_")
+	return code.toLowerCase().replace(/-/, '_');
 }
 
 function getPreferredLanguage(options) {
-  if (!options) {
-    return getBrowserLang()
-  }
+	if (!options) {
+		return getBrowserLang();
+	}
 
-  const { languages, fallback } = options
-  if (!options.languages) {
-    return fallback
-  }
+	const { languages, fallback } = options;
+	if (!options.languages) {
+		return fallback;
+	}
 
-  // some browsers report language as en-US instead of en_US
-  const browserLanguage = normalizeCode(getBrowserLang())
+	// some browsers report language as en-US instead of en_US
+	const browserLanguage = normalizeCode(getBrowserLang());
 
-  if (!browserLanguage) {
-    return fallback
-  }
+	if (!browserLanguage) {
+		return fallback;
+	}
 
-  const match = languages.filter(
-    lang => normalizeCode(lang) === browserLanguage
-  )
+	const match = languages.filter((lang) => normalizeCode(lang) === browserLanguage);
 
-  if (match.length > 0) {
-    return match[0] || fallback
-  }
+	if (match.length > 0) {
+		return match[0] || fallback;
+	}
 
-  // en == en_US
-  const matchCodeOnly = languages.filter(lang =>
-    startsWith(browserLanguage, lang) || startsWith(lang, browserLanguage)
-  )
-  return matchCodeOnly[0] || fallback
+	// en == en_US
+	const matchCodeOnly = languages.filter(
+		(lang) => startsWith(browserLanguage, lang) || startsWith(lang, browserLanguage)
+	);
+	return matchCodeOnly[0] || fallback;
 }
 
-export default getPreferredLanguage
+export default getPreferredLanguage;
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,104 +1,104 @@
-import browserLang from "../src"
+import browserLang from '../src';
 
-const mockNavigator = obj => {
-  // eslint-disable-next-line no-restricted-syntax
-  for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      Object.defineProperty(window.navigator, key, {
-        value: obj[key],
-        configurable: true,
-      })
-    }
-  }
-}
+const mockNavigator = (obj) => {
+	// eslint-disable-next-line no-restricted-syntax
+	for (const key in obj) {
+		if (Object.prototype.hasOwnProperty.call(obj, key)) {
+			Object.defineProperty(window.navigator, key, {
+				value: obj[key],
+				configurable: true,
+			});
+		}
+	}
+};
 
 const initNavigator = () => {
-  mockNavigator({
-    languages: undefined,
-    language: undefined,
-    browserLanguage: undefined,
-    userLanguage: undefined,
-  })
-}
+	mockNavigator({
+		languages: undefined,
+		language: undefined,
+		browserLanguage: undefined,
+		userLanguage: undefined,
+	});
+};
 
-describe("browserLang", () => {
-  initNavigator()
+describe('browserLang', () => {
+	initNavigator();
 
-  it("should return null when navigator.languages is undefined.", () => {
-    expect(browserLang()).toBe(null)
-  })
+	it('should return null when navigator.languages is undefined.', () => {
+		expect(browserLang()).toBe(null);
+	});
 
-  it('should return "ko_KR" when navigator.languages is defined as ["ko_KR", "ko"]', () => {
-    mockNavigator({
-      languages: ["ko_KR", "ko"],
-    })
-    expect(browserLang()).toBe("ko_KR")
-  })
+	it('should return "ko_KR" when navigator.languages is defined as ["ko_KR", "ko"]', () => {
+		mockNavigator({
+			languages: ['ko_KR', 'ko'],
+		});
+		expect(browserLang()).toBe('ko_KR');
+	});
 
-  it('should return "ko_KR" when navigator.browserLanguage is defined as "ko_KR"', () => {
-    mockNavigator({
-      browserLanguage: "ko_KR",
-    })
-    expect(browserLang()).toBe("ko_KR")
-  })
+	it('should return "ko_KR" when navigator.browserLanguage is defined as "ko_KR"', () => {
+		mockNavigator({
+			browserLanguage: 'ko_KR',
+		});
+		expect(browserLang()).toBe('ko_KR');
+	});
 
-  it("should return fallback when navigator.languages is undefined.", () => {
-    const fallback = "ko_KR"
-    const options = {
-      fallback,
-    }
-    expect(browserLang(options)).toBe(fallback)
-  })
+	it('should return fallback when navigator.languages is undefined.', () => {
+		const fallback = 'ko_KR';
+		const options = {
+			fallback,
+		};
+		expect(browserLang(options)).toBe(fallback);
+	});
 
-  it("should return fallback when there is no available language on the list.", () => {
-    const fallback = "ko_KR"
-    mockNavigator({
-      languages: ["de"],
-    })
-    const options = {
-      languages: ["fr", "ko", "en"],
-      fallback,
-    }
-    expect(browserLang(options)).toBe(fallback)
-  })
+	it('should return fallback when there is no available language on the list.', () => {
+		const fallback = 'ko_KR';
+		mockNavigator({
+			languages: ['de'],
+		});
+		const options = {
+			languages: ['fr', 'ko', 'en'],
+			fallback,
+		};
+		expect(browserLang(options)).toBe(fallback);
+	});
 
-  it('should return "zh_TW" when navigator.languages is ["zh_TW"] and "zh_TW" is on the list.', () => {
-    mockNavigator({
-      languages: ["zh_TW"],
-    })
-    const options = {
-      languages: ["zh", "zh_TW", "en"],
-    }
-    expect(browserLang(options)).toBe("zh_TW")
-  })
+	it('should return "zh_TW" when navigator.languages is ["zh_TW"] and "zh_TW" is on the list.', () => {
+		mockNavigator({
+			languages: ['zh_TW'],
+		});
+		const options = {
+			languages: ['zh', 'zh_TW', 'en'],
+		};
+		expect(browserLang(options)).toBe('zh_TW');
+	});
 
-  it('should return "zh" when navigator.languages is ["zh_TW"] and only "zh" is on the list.', () => {
-    mockNavigator({
-      languages: ["zh_TW"],
-    })
-    const options = {
-      languages: ["zh", "en"],
-    }
-    expect(browserLang(options)).toBe("zh")
-  })
+	it('should return "zh" when navigator.languages is ["zh_TW"] and only "zh" is on the list.', () => {
+		mockNavigator({
+			languages: ['zh_TW'],
+		});
+		const options = {
+			languages: ['zh', 'en'],
+		};
+		expect(browserLang(options)).toBe('zh');
+	});
 
-  it('should return "de-de" when navigator.languages is ["de"] and "de-de" is one item in the list.', () => {
-    mockNavigator({
-      languages: ["de"],
-    })
-    const options = {
-      languages: ["en-us", "de-de"],
-    }
-    expect(browserLang(options)).toBe("de-de")
-  })
+	it('should return "de-de" when navigator.languages is ["de"] and "de-de" is one item in the list.', () => {
+		mockNavigator({
+			languages: ['de'],
+		});
+		const options = {
+			languages: ['en-us', 'de-de'],
+		};
+		expect(browserLang(options)).toBe('de-de');
+	});
 
-  it('should return "de-de" when navigator.languages is ["de"] and "de-de" is first item in the list.', () => {
-    mockNavigator({
-      languages: ["de"],
-    })
-    const options = {
-      languages: ["en-us", "de-de", "de-as"],
-    }
-    expect(browserLang(options)).toBe("de-de")
-  })
-})
+	it('should return "de-de" when navigator.languages is ["de"] and "de-de" is first item in the list.', () => {
+		mockNavigator({
+			languages: ['de'],
+		});
+		const options = {
+			languages: ['en-us', 'de-de', 'de-as'],
+		};
+		expect(browserLang(options)).toBe('de-de');
+	});
+});
